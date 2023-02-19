aed24d7d781c4bfc3c2d6d2fe363525a82a445c9
caiobraza <caio.lg.g2@gmail.com>
2023-02-19 19:12:24 -0300

    primeiro commit

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author caio brasil
+ *
+ */
+module variaveis {
+}
---
+++ src/variaveis/Tipos_de_variaveis.java
@@ -0,0 +1,32 @@
+package variaveis;
+
+public class Tipos_de_variaveis {
+
+	public static void main(String[] args) {
+		/* Tipos de variaveis -variavel, que é
+		   simplesmente um espaço na memória
+		   o qual reservamos e damos um nome.
+		 */
+		
+		/* Variaveis do tipo
+		 * inteiro
+		 * real
+		 * string
+		 * lógico
+		 */
+		
+		Double salarioDoFuncionario = 1300.50;
+		Double bonusMensal = 250.00;
+		Double horas = 2.0;
+		
+		Double saldoDaConta = (salarioDoFuncionario + bonusMensal) * horas;
+		
+		System.out.println("O valor do saldo é: " + saldoDaConta);
+		
+		
+		
+		
+	
+	}
+
+}
